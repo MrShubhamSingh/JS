40cc69ad8824661ad537684b5f0b6e4b0347d3ce
“Shubham <shubhamitd237@gmail.com>
2023-03-21 20:48:42 +0530

    Note app start

--- app/index.js
+++ app/index.js
@@ -1,22 +1,20 @@
-
-// function getActiveElement(){
-//     console.log(document.activeElement);
-//     const activeTag = document.activeElement.tagName;
-//     document.getElementById("content").innerHTML = activeTag;
-// }
-
-
-function pressSwitch(){
-   var switchText  = document.getElementById("content").innerHTML;
-    if(switchText==="Switch Off"){
-        document.getElementById("content").innerHTML="Switch On"
-        document.getElementById("bulb").innerHTML =  "<img width=\"100\" height=\"100\" src=\"https://www.pngfind.com/pngs/m/80-805591_light-bulb-icon-png-transparent-png.png\">"
+function addList(){
+    let title = document.getElementById('title').value;
+    if(title){
+        let listItem = document.createElement("li");
+        listItem.innerText = title;
+        let listContainer = document.getElementById('list');
+        listContainer.appendChild(listItem);
+        console.log(listContainer)
     }
     else{
-        document.getElementById("content").innerHTML="Switch Off"
-        document.getElementById("bulb").innerHTML =  "<img width=\"100\" height=\"100\" src=\"https://static.wixstatic.com/media/2cd43b_31844ce00cf04315a0e69681e3f97740~mv2.png/v1/fill/w_256,h_256,q_90/2cd43b_31844ce00cf04315a0e69681e3f97740~mv2.png\">"
+        alert("Please enter the title")
     }
 }
-// document.getElementById("content").addEventListener("click",()=>{
-//     console.log("Div is clicked")
-// })
+
+function deleteHandler(){
+    let deleteItem = document.querySelectorAll('.delete')
+    for(let i=0; i<deleteItem.length; i++){
+        deleteItem[i].parentElement.style.display = 'none';
+    }
+}
